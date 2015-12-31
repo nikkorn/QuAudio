f9d9234c6f641919f5862182e00f16a84f140aad
nikkorn <nikhoward1234@hotmail.co.uk>
2015-12-31 15:14:57 +0000

    Started on ActionChannel for the QuClient
    Started on implementing the ActionChannel for the QuClient which will be
    used to maintain a connection to the QuServer as well as
    sending/receiving action requests/responses.

--- QuClient/src/Server/Device.java
+++ QuClient/src/Server/Device.java
@@ -1,5 +1,40 @@
 package Server;
 
-public class Device {
+import NetProbe.ReachableQuDevice;
 
+/**
+ * Represents an instance of a Qu Server
+ * @author Nikolas Howard
+ *
+ */
+public class Device {
+	private ReachableQuDevice reachableDevice;
+	
+	public Device(ReachableQuDevice reachableDevice) {
+		this.reachableDevice = reachableDevice;
+	}
+	
+	public String getDeviceId() {
+		return reachableDevice.getDeviceId();
+	}
+
+	public String getAddress() {
+		return reachableDevice.getAddress();
+	}
+
+	public boolean isProtected() {
+		return reachableDevice.isProtected();
+	}
+
+	public String getDeviceName() {
+		return reachableDevice.getDeviceName();
+	}
+
+	public int getAudioFileReceiverPort() {
+		return reachableDevice.getAudioFileReceiverPort();
+	}
+
+	public int getClientManagerPort() {
+		return reachableDevice.getClientManagerPort();
+	}
 }
---
+++ QuClient/src/Server/IncomingAction.java
@@ -0,0 +1,23 @@
+package Server;
+
+import org.json.JSONObject;
+
+/**
+ * Represents an action sent by the server
+ * @author Nikolas Howard
+ *
+ */
+public class IncomingAction extends Action {
+	
+	public IncomingAction(JSONObject infoObject) {
+		this.actionInfoObject = infoObject;
+	}
+	
+	/**
+	 * Get the IncomingActionType of this Action.
+	 * @return IncomingActionType
+	 */
+	public IncomingActionType getIncomingActionType() {
+		return IncomingActionType.valueOf(this.getActionInfoObject().getString("action_type"));
+	}
+}
